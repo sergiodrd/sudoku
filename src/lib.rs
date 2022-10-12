ab3b8d4c704a9bd0de43e5c81885c3ff31e4cc04
sergio <sergiod.roddj@gmail.com>
2022-10-12 11:04:28 -0400

    implemented position struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Pos {
+    x: u8,
+    y: u8,
+}
+
+impl Pos {
+    pub fn new(x: u8, y: u8) -> Self {
+        if x > 8 || y > 8 {
+            panic!("Position out of bounds.");
+        }
+        Self { x, y }
+    }
+    pub fn to_index(&self) -> usize {
+        (self.y * 9 + self.x) as usize
+    }
+    pub fn x(&self) -> u8 {
+        self.x
+    }
+    pub fn y(&self) -> u8 {
+        self.y
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +27,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn pos_converts_to_index() {
+        let p = Pos::new(5, 3);
+        assert_eq!(p.to_index(), 32);
     }
 }

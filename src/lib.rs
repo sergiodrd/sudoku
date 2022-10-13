c2b3c7c34943c38deb5b5f7f8e29077ee05846dc
sergio <sergiod.roddj@gmail.com>
2022-10-13 12:20:29 -0400

    implemented get_constraints for Cell

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Pos {
     x: u8,
@@ -51,9 +53,11 @@ impl Cell {
     }
     pub fn get_constraints<'a>(&self, board: &'a Sudoku) -> impl Iterator<Item = u8> + 'a {
         board
-            .iter()
-            .filter(|c| matches!(c.value, Some(_)))
-            .map(|c| c.value.unwrap())
+            .get_rest_of_row(self.position)
+            .chain(board.get_rest_of_column(self.position))
+            .chain(board.get_rest_of_box(self.position))
+            .collect::<HashSet<_>>()
+            .into_iter()
     }
 }
 
@@ -119,7 +123,9 @@ impl std::str::FromStr for Sudoku {
             if c.is_ascii_digit() {
                 let c = c.to_digit(10).unwrap();
                 c == 0 || c > 9
-            } else { c != '.' }
+            } else {
+                c != '.'
+            }
         }) {
             return Err("Sudoku str contains invalid characters.");
         }
@@ -142,7 +148,7 @@ impl std::str::FromStr for Sudoku {
                     _ => unreachable!(),
                 })
                 .collect(),
-            })
+        })
     }
 }
 
@@ -154,35 +160,63 @@ mod tests {
 
     #[test]
     fn pos_converts_to_index() {
-        let p = Pos::new(5, 3);
-        assert_eq!(p.to_index(), 32);
+        assert_eq!(Pos::new(5, 3).to_index(), 32);
     }
 
     #[test]
     fn pos_from_index_is_correct() {
-        let p = Pos::from_index(32);
-        let p2 = Pos::new(5, 3);
-        assert_eq!(p, p2);
+        assert_eq!(Pos::from_index(32), Pos::new(5, 3));
     }
 
     #[test]
     fn sudoku_can_get_rest_of_row() {
-        let s = Sudoku::from_str(".5..83.17...1..4..3.4..56.8....3...9.9.8245....6....7...9....5...729..861.36.72.4").unwrap();
-        let rest_of_row = vec![9u8, 8, 2, 5];
-        assert_eq!(s.get_rest_of_row(Pos::new(5, 4)).collect::<Vec<_>>(), rest_of_row);
+        let s = Sudoku::from_str(
+            ".5..83.17...1..4..3.4..56.8....3...9.9.8245....6....7...9....5...729..861.36.72.4",
+        )
+        .unwrap();
+        assert_eq!(
+            s.get_rest_of_row(Pos::new(5, 4)).collect::<Vec<_>>(),
+            vec![9u8, 8, 2, 5]
+        );
     }
 
     #[test]
     fn sudoku_can_get_rest_of_column() {
-        let s = Sudoku::from_str(".5..83.17...1..4..3.4..56.8....3...9.9.8245....6....7...9....5...729..861.36.72.4").unwrap();
-        let rest_of_column = vec![3u8, 4, 7];
-        assert_eq!(s.get_rest_of_column(Pos::new(5, 2)).collect::<Vec<_>>(), rest_of_column);
+        let s = Sudoku::from_str(
+            ".5..83.17...1..4..3.4..56.8....3...9.9.8245....6....7...9....5...729..861.36.72.4",
+        )
+        .unwrap();
+        assert_eq!(
+            s.get_rest_of_column(Pos::new(5, 2)).collect::<Vec<_>>(),
+            vec![3u8, 4, 7]
+        );
     }
 
     #[test]
     fn sudoku_can_get_rest_of_box() {
-        let s = Sudoku::from_str(".5..83.17...1..4..3.4..56.8....3...9.9.8245....6....7...9....5...729..861.36.72.4").unwrap();
-        let rest_of_box = vec![1u8, 7, 4, 6, 8];
-        assert_eq!(s.get_rest_of_box(Pos::new(7, 1)).collect::<Vec<_>>(), rest_of_box);
+        let s = Sudoku::from_str(
+            ".5..83.17...1..4..3.4..56.8....3...9.9.8245....6....7...9....5...729..861.36.72.4",
+        )
+        .unwrap();
+        assert_eq!(
+            s.get_rest_of_box(Pos::new(7, 1)).collect::<Vec<_>>(),
+            vec![1u8, 7, 4, 6, 8]
+        );
+    }
+
+    #[test]
+    fn correct_constraints_from_position() {
+        let s = Sudoku::from_str(
+            ".5..83.17...1..4..3.4..56.8....3...9.9.8245....6....7...9....5...729..861.36.72.4",
+        )
+        .unwrap();
+        let mut constraints = s
+            .iter()
+            .find(|c| c.position == Pos::new(7, 1))
+            .unwrap()
+            .get_constraints(&s)
+            .collect::<Vec<_>>();
+        constraints.sort();
+        assert_eq!(constraints, vec![1u8, 4, 5, 6, 7, 8]);
     }
 }
